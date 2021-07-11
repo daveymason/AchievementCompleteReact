dca03c0ead2dc08e1430d51d651b0a2c21f5f3cf
Davey Mason <daveymason@outlook.com>
2021-07-11 15:37:41 +0800

    Updated links and prettified code

--- src/components/structure/Form.js
+++ src/components/structure/Form.js
@@ -4,44 +4,42 @@ import CForm from "./CForm.js";
 
 const Form = () => {
   return (
-    <div className="contactBg pt-sm-3 pb-sm-4">
-      <Container fluid>
-        <div className="row text-center">
-          <div className="col mb-3">
-            <h2 className="display-4">
-              <span className="blueText font-weight-bold">C</span>ontact{" "}
-              <span className="orangeText font-weight-bold">U</span>s
-            </h2>
+    <div class="anchorDiv py-2" id="contact">
+      <div className="contactBg pt-sm-3 pb-sm-4">
+        <Container fluid>
+          <div className="row text-center">
+            <div className="col mb-3">
+              <h2 className="display-4">
+                <span className="blueText font-weight-bold">C</span>ontact{" "}
+                <span className="orangeText font-weight-bold">U</span>s
+              </h2>
+            </div>
           </div>
-        </div>
-        <div className="row text-right">
-          <div className="col-sm-6 whiteOverlay mx-auto">
-            <CForm/>
+          <div className="row text-right">
+            <div className="col-sm-6 whiteOverlay mx-auto">
+              <CForm />
+            </div>
           </div>
-        </div>
-        <div className="row">
-          <div className="col text-center mt-2 mx-auto">
-            <button type="button" className="btn mx-5" title="cntButtonEmail">
-              <a
-                href="mailto:daveymason@outlook.com"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <i className="far fa-envelope display-1"></i>
-              </a>
-            </button>
-            <button type="button" className="btn mx-5" title="cntButtonEmail">
-              <a
-                href="tel:123-456-7890"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <i className="fa fa-phone display-1"></i>
-              </a>
-            </button>
+          <div className="row">
+            <div className="col text-center mt-2 mx-auto">
+              <button type="button" className="btn mx-5" title="cntButtonEmail">
+                <a
+                  href="mailto:daveymason@outlook.com"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <i className="far fa-envelope display-1"></i>
+                </a>
+              </button>
+              <button type="button" className="btn mx-5" title="cntButtonEmail">
+                <a href="tel:123-456-7890" target="_blank" rel="noreferrer">
+                  <i className="fa fa-phone display-1"></i>
+                </a>
+              </button>
+            </div>
           </div>
-        </div>
-      </Container>
+        </Container>
+      </div>
     </div>
   );
 };
