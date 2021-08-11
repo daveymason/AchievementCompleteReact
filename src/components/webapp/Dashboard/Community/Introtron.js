b34ffbecb267560af5d78e1c1cfdadda813e3eab
Davey Mason <daveymason@outlook.com>
2021-08-11 21:58:14 +0800

    Fix overlap

--- src/components/webapp/Dashboard/Community/Introtron.js
+++ src/components/webapp/Dashboard/Community/Introtron.js
@@ -4,7 +4,7 @@ import { Container } from 'reactstrap';
 const Intro = () => {
   return (
     <Container fluid className="commBg">
-      <div class="row align-items-end mt-5">
+      <div class="row mt-5 noMarginY">
         <div class="col-sm-10 col-md-7 col-lg-6 text-center mx-auto my-5 py-3 py-sm-4 blackOverlay text-white">
           <h1 class="display-4 font-weight-bold py-2">
             <span class="orangeText">C</span>ommunity
