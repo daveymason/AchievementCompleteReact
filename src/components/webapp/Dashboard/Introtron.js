f0a0fa9d03d2150dfd52706fe20cae659b070971
Davey Mason <daveymason@outlook.com>
2021-08-14 00:07:37 +0800

    Update Introtron.js

--- src/components/webapp/Dashboard/Introtron.js
+++ src/components/webapp/Dashboard/Introtron.js
@@ -4,13 +4,13 @@ import { Container } from 'reactstrap';
 const Intro = () => {
   return (
     <Container fluid className="dashBg">
-      <div class="row mt-5">
+      <Row>
         <div class="col-sm-10 col-md-7 col-lg-6 text-center mx-auto my-5 py-3 py-sm-4 blackOverlay text-white">
           <h1 class="display-4 font-weight-bold py-2">
             <span class="orangeText">D</span>ashboard
           </h1>
         </div>
-      </div>
+      </Row>
     </Container>
   );
 };
